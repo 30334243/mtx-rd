14d991884d483c47e664264ca659b49981b71c06
bvs <fabish@bk.ru>
2024-12-22 13:29:46 +0300

    1. Добавлен preset для mt и md 2. Изменена dll, возвращает всегда строку (без рандома) 3. Имя dll теперь берется из имени проекта при динамической загрузки в app

--- lib/include/dll.h
+++ lib/include/dll.h
@@ -1,13 +1,11 @@
 #pragma once
-#include <random>
 #include <string>
 
-struct Process {
+struct Process
+{
     bool Run(int num);
     [[nodiscard]] std::string Info();
 
 private:
-    std::random_device r{};
-    std::default_random_engine e1{r()};
     std::string str{};
 };
--- lib/src/dll.cpp
+++ lib/src/dll.cpp
@@ -1,18 +1,16 @@
 #include "dll.h"
 
-bool Process::Run(int const num) {
+bool Process::Run(int const num)
+{
     str.clear();
-    for (int i{}; i < num; ++i) {
-        std::uniform_int_distribution<int> uniform_dist(1, 100);
-        auto const val{uniform_dist(e1)};
-        if (val % 2) {
-            return false;
-        }
-        str.append(std::to_string(val));
+    for (int i{}; i < num; ++i)
+    {
+        str.append(std::to_string(i));
     }
     return true;
 }
 
-std::string Process::Info() {
-    return str;
+std::string Process::Info()
+{
+    return str + "result";
 }
